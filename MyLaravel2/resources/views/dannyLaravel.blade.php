c14efac4edbe2762ce675d96f68ff140010f3aca
Danny <danny.brogno@gmail.com>
2021-05-14 17:59:27 +0200

    Project Done

---
+++ MyLaravel2/resources/views/dannyLaravel.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+    <title>TEST</title>
+  </head>
+  <body>
+
+    <header>
+      <h1>HELLO WORLD!1!1!1!1!</h1>
+    </header>
+
+  </body>
+</html>
